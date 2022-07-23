06bcddb7229e850e886ff32d8b8a5d017c96d30d
Rana128 <87471725+Rana128@users.noreply.github.com>
2022-07-24 02:32:46 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open_menu_btn");
+const closeBtn = document.querySelector("#close_menu_btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav)
+
+
+// faq ans
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('Open');
+
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus"
+        }else{
+            icon.className = "uil uil-plus";
+        }
+    })
+});
+
+//Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        600:{
+            slidesPerView: 2,
+        }
+    }
+  });
